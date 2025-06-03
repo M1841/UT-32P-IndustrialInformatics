d1eaa351a2a2b03a80d90b4ba87a552ddb49cd97
Hestray <barzanemma@gmail.com>
2025-06-03 12:03:17 +0300

    damage control finished; proper css and fix of medications and prescriptions - work in progress

--- EMR_BMED.Frontend/src/app/app.component.ts
+++ EMR_BMED.Frontend/src/app/app.component.ts
@@ -5,49 +5,50 @@ import { ApiService } from './services/api/api.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
-  template: `
-    @if (isAuthenticated() && user() != null) {
-      <div class="page-top">
-        <div class="header">
-          <img src="Header.png" alt="Header" id="header-pic" />
-          <div class="image-container"></div>
-        </div>
-        <div class="nav">
-          <a [routerLink]="['']" class="nav-button">Home</a> <br />
-          <a [routerLink]="['prescriptions']" class="nav-button"
-            >Prescriptions</a
-          >
-          <br />
-          <a [routerLink]="['medication']" class="nav-button">Medication</a>
-          <br />
-          <a
-            [routerLink]="[user()!.isDoctor ? 'profile/doctor' : 'profile']"
-            class="nav-button"
-            >Profile</a
-          >
-          <br />
-          <button class="logout-btn" (click)="handleLogout()">Logout</button>
-        </div>
-      </div>
-    }
-    <router-outlet />
+  templateUrl: './app.component.html',
+  // template: `
+  //   @if (isAuthenticated() && user() != null) {
+  //     <div class="page-top">
+  //       <div class="header">
+  //         <img src="Header.png" alt="Header" id="header-pic" />
+  //         <div class="image-container"></div>
+  //       </div>
+  //       <div class="nav">
+  //         <a [routerLink]="['']" class="nav-button">Home</a> <br />
+  //         <a [routerLink]="['prescriptions']" class="nav-button"
+  //           >Prescriptions</a
+  //         >
+  //         <br />
+  //         <a [routerLink]="['medication']" class="nav-button">Medication</a>
+  //         <br />
+  //         <a
+  //           [routerLink]="[user()!.isDoctor ? 'profile/doctor' : 'profile']"
+  //           class="nav-button"
+  //           >Profile</a
+  //         >
+  //         <br />
+  //         <button class="logout-btn" (click)="handleLogout()">Logout</button>
+  //       </div>
+  //     </div>
+  //   }
+  //   <router-outlet />
 
-    <div class="page-bottom">
-      <div class="footer">
-        <div class="icon">
-          <a [routerLink]="['/']" class="home-icon" style="grid-area: box-1">
-            <img src="Logo.png" alt="Home" width="96" height="96" />
-          </a>
-          <p id="rights" style="grid-area: box-2">All rights reserved.</p>
-        </div>
-        <div class="info" style="grid-area: box-3">
-          <div>Outer links</div>
-          <div>Inner links</div>
-          <div>Contact info</div>
-        </div>
-      </div>
-    </div>
-  `,
+  //   <div class="page-bottom">
+  //     <div class="footer">
+  //       <div class="icon">
+  //         <a [routerLink]="['/']" class="home-icon" style="grid-area: box-1">
+  //           <img src="Logo.png" alt="Home" width="96" height="96" />
+  //         </a>
+  //         <p id="rights" style="grid-area: box-2">All rights reserved.</p>
+  //       </div>
+  //       <div class="info" style="grid-area: box-3">
+  //         <div>Outer links</div>
+  //         <div>Inner links</div>
+  //         <div>Contact info</div>
+  //       </div>
+  //     </div>
+  //   </div>
+  // `,
 })
 export class AppComponent {
   readonly user = signal<{
--- EMR_BMED.Frontend/src/app/pages/profile/doctor/doctor-profile.component.ts
+++ EMR_BMED.Frontend/src/app/pages/profile/doctor/doctor-profile.component.ts
@@ -6,53 +6,54 @@ import { ApiService } from '@/services/api/api.service';
 @Component({
   selector: 'app-doctor-profile',
   imports: [RouterLink],
-  template: `
-    <div class="profile-container">
-      <a href="/" class="home-icon">
-        <img src="Logo.png" alt="Home" width="64" height="64" />
-      </a>
+  templateUrl: './doctor-profile.html',
+  // template: `
+  //   <div class="profile-container">
+  //     <a href="/" class="home-icon">
+  //       <img src="Logo.png" alt="Home" width="64" height="64" />
+  //     </a>
 
-      <h2>Doctor Profile</h2>
-      <div class="profile-details">
-      @if (doctor()) {
-        <p><strong>Name:</strong> Dr. {{ doctor()!.name }} {{ doctor()!.surname }}</p>
-        <p><strong>Medical Field:</strong> {{ doctor()!.medicalField }}</p>
-        <p><strong>Email address:</strong> {{ doctor()!.email }}</p>
-        <p><strong>Gender:</strong> {{ doctor()!.gender }}</p>
-        <p><strong>Birthday:</strong> {{ doctor()!.birthday }}</p>
-        @if (doctor()!.phone) {
-          <p><strong>Phone number:</strong> {{ doctor()!.phone }}</p>
-        }
-        @if (doctor()!.address) {
-          <p><strong>Address:</strong> {{ doctor()!.address }}</p>
-        }
+  //     <h2>Doctor Profile</h2>
+  //     <div class="profile-details">
+  //     @if (doctor()) {
+  //       <p><strong>Name:</strong> Dr. {{ doctor()!.name }} {{ doctor()!.surname }}</p>
+  //       <p><strong>Medical Field:</strong> {{ doctor()!.medicalField }}</p>
+  //       <p><strong>Email address:</strong> {{ doctor()!.email }}</p>
+  //       <p><strong>Gender:</strong> {{ doctor()!.gender }}</p>
+  //       <p><strong>Birthday:</strong> {{ doctor()!.birthday }}</p>
+  //       @if (doctor()!.phone) {
+  //         <p><strong>Phone number:</strong> {{ doctor()!.phone }}</p>
+  //       }
+  //       @if (doctor()!.address) {
+  //         <p><strong>Address:</strong> {{ doctor()!.address }}</p>
+  //       }
 
-        <a [routerLink]="['edit']" class="nav-button">Edit</a>
+  //       <a [routerLink]="['edit']" class="nav-button">Edit</a>
 
-        <button (click)="openDeleteDialog()" class="logout-btn">
-          Delete Account
-        </button>
-        <dialog>
-          <form method="dialog">
-            <p>
-              Are you sure you want to delete your account? This action is
-              permanent!
-            </p>
-            <menu style="padding:0">
-              <button class="nav-button">No, cancel</button>
-              <button (click)="handleDelete()" class="logout-btn">
-                Yes, delete
-              </button>
-            </menu>
-          </form>
-        </dialog>
-      } @else {
-        <p>Loading user details...</p>
-      }
-    </div>
-    </div>
-  `,
-  styles: ``,
+  //       <button (click)="openDeleteDialog()" class="logout-btn">
+  //         Delete Account
+  //       </button>
+  //       <dialog>
+  //         <form method="dialog">
+  //           <p>
+  //             Are you sure you want to delete your account? This action is
+  //             permanent!
+  //           </p>
+  //           <menu style="padding:0">
+  //             <button class="nav-button">No, cancel</button>
+  //             <button (click)="handleDelete()" class="logout-btn">
+  //               Yes, delete
+  //             </button>
+  //           </menu>
+  //         </form>
+  //       </dialog>
+  //     } @else {
+  //       <p>Loading user details...</p>
+  //     }
+  //   </div>
+  //   </div>
+  // `,
+  // styles: ``,
 })
 export class DoctorProfileComponent {
   readonly doctor = signal<any>({});
